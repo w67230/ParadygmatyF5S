4731414c7bf00dca95663a3876d8dcd430cd5b8f
Xires87 <frycmod@gmail.com>
2024-11-14 15:22:54 +0100

    4 zadanie dokonczone

--- ParadygmatyF5S/lab4/4.fs
+++ ParadygmatyF5S/lab4/4.fs
@@ -6,6 +6,7 @@ open System.Collections.Generic;
 type Konto private (id : string, imie : string) = 
     static let ids = new Dictionary<string, Konto>();
     let imie = imie;
+    let id = id;
     let mutable kwota = 0.0;
 
     static member StworzKonto(id :string, imie : string) : Option<Konto> = 
@@ -39,17 +40,67 @@ type Konto private (id : string, imie : string) =
     member this.WyswietlIloscPieniedzy() =
         printfn("Ilosc pieniedzy na koncie: %f") kwota;
 
+    member this.Imie = imie;
+    member this.Id = id;
+
+
+let rec wybierzKonto() : Konto = 
+    printfn("Wybierz co chcesz zrobic:");
+    printfn("1 - Wejdz na konto");
+    printfn("2 - Stworz nowe konto");
+    let wybor = Console.ReadLine();
+
+    if(wybor = "1") then
+        printfn("Wpisz id konta na które chcesz wejść");
+        let id = Console.ReadLine();
+        if(Konto.GetIds().ContainsKey(id)) then
+            Konto.GetKonto(id);
+        else
+            printfn("Nie znaleziono podanego konta");
+            wybierzKonto();
+    else if(wybor = "2") then
+        printfn("Wpisz id konta ktore chcesz utworzyc");
+        let id = Console.ReadLine();
+        printfn("Wpisz swoje imie");
+        let imie = Console.ReadLine();
+        let konto = Konto.StworzKonto(id, imie);
+        if(konto.IsSome) then
+            konto.Value;
+        else
+            wybierzKonto();
+    else wybierzKonto();
+
+let rec wybierzAkcje(konto : Konto) =
+    printfn("Witaj %s. Wybierz co chcesz zrobic:") konto.Imie;
+    printfn("1 - Wyswietl dostepne srodki");
+    printfn("2 - Wplata");
+    printfn("3 - Wyplata");
+    let wybor = Console.ReadLine();
+
+    if(wybor = "1") then
+        konto.WyswietlIloscPieniedzy();
+    else if(wybor = "2") then
+        printfn("Podaj sume jaka chcesz wplacic");
+        let kwota = (float)(Console.ReadLine());
+        konto.Wplac(kwota);
+    else if(wybor = "3") then
+        printfn("Podaj sume jaka chcesz wyplacic");
+        let kwota = (float)(Console.ReadLine());
+        konto.Wyplac(kwota);
+
+    wybierzAkcje(konto);
+
+
 
 let main() = 
-    // TODO dac tu menu zeby sie klikalo i mozna bylo se tworzyc itp
     let ktos = Konto.StworzKonto("cos", "tam");
-    let tam = Konto.StworzKonto("cos", "nie");
+    let tam = Konto.StworzKonto("nie", "nie");
     if(ktos.IsSome) then
         ktos.Value.Wplac(54);
-        ktos.Value.WyswietlIloscPieniedzy();
-        ktos.Value.Wyplac(60);
-        ktos.Value.WyswietlIloscPieniedzy();
-        ktos.Value.Wyplac(25);
-        ktos.Value.WyswietlIloscPieniedzy();
+
+    
+    let konto = wybierzKonto();
+    wybierzAkcje(konto);
+
 
     0;
